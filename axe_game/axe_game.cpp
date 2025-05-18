76d785afc61af4a1df8c7aea9fd766a1bf082cb1
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2025-05-18 19:16:57 +1000

    while Loop and loops

--- axe_game/axe_game.cpp
+++ axe_game/axe_game.cpp
@@ -2,21 +2,12 @@
 
 int main()
 {
-	// initialize window
-	// false
-	bool equal{ 4 == 9 };
-	// true
-	bool notEqual{ 4 != 9 };
-	// true
-	bool less{ 4 < 9 };
-	// false
-	bool greater{ 4 > 9 };
-	// true
-	bool lessEqual{ 4 <= 9 };
-	// false
-	bool greaterEqual{ 4 >= 9 };
-
 	int width;
 	width = 350;
 	InitWindow(350, 200, "Rama's Window!");
+
+	while (true)
+	{
+
+	}
 }
