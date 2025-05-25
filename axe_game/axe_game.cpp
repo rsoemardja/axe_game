54fb20d0b80dd54cf03386699ba84a1fbfe44072
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2025-05-25 20:35:19 +1000

    Used && to give collision_with_axe the correct values its still buggy done the logic

--- axe_game/axe_game.cpp
+++ axe_game/axe_game.cpp
@@ -31,8 +31,13 @@ int main()
 
 	int direction{ 10 };
 
-	bool collision_with_axe{ false };
+	bool collision_with_axe = 
+						(b_axe_y >= u_circle_y) && 
+						(u_axe_y <= b_circle_y) && 
+						(r_axe_x >= l_circle_x) && 
+						(l_axe_x <= r_circle_x);
 
+	SetTargetFPS(60);
 	while (WindowShouldClose() != true)
 	{
 		
@@ -45,27 +50,51 @@ int main()
 			}
 			else
 			{
-			// Game logic
+				// Game logic begins here
 
-			DrawCircle(circle_x, circle_y, 50, GREEN);
-			DrawRectangle(axe_x, axe_y, axe_length, 50, BLUE);
+				// update the edges
+				// left and right edges of the axe
+				l_circle_x = circle_x - circle_radius;
+				r_circle_x = circle_x + circle_radius;
+				// upper and bottom edges of the axe
+				u_circle_y = circle_y - circle_radius;
+				b_circle_y = circle_y + circle_radius;
 
-			// Move the axe
-			axe_y += direction;
-			if (axe_y > height || axe_y < 0)
-			{
-				direction = -direction;
-			}
+				// left and right edges of the axe
+				l_axe_x = axe_x;
+				// right edge of the axe
+				r_axe_x = axe_x + axe_length;
+				// upper and bottom edges of the axe
+				u_axe_y = axe_y;
+				// bottom edge of the axe
+				b_axe_y = axe_y + axe_length;
+				
+				// Update collision_with_axe
+				collision_with_axe =
+					(b_axe_y >= u_circle_y) &&
+					(u_axe_y <= b_circle_y) &&
+					(r_axe_x >= l_circle_x) &&
+					(l_axe_x <= r_circle_x);
 
-			// direct the axe and collision detection
-			if (IsKeyDown(KEY_D) && circle_x < width)
-			{
-				circle_x += 10;
-			}
-			if (IsKeyDown(KEY_A) && circle_x < 0)
-			{
-				circle_x -= 10;
-			}
+				DrawCircle(circle_x, circle_y, 50, GREEN);
+				DrawRectangle(axe_x, axe_y, axe_length, 50, BLUE);
+
+				// Move the axe
+				axe_y += direction;
+				if (axe_y > height || axe_y < 0)
+				{
+					direction = -direction;
+				}
+
+				// direct the axe and collision detection
+				if (IsKeyDown(KEY_D) && circle_x < width)
+				{
+					circle_x += 10;
+				}
+				if (IsKeyDown(KEY_A) && circle_x < 0)
+				{
+					circle_x -= 10;
+				}
 
 			// Game Logic Ends here
 		}
